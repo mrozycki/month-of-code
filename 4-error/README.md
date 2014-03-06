2d56c4e2fcb28332456bc8aa5c5aa2c7085a21b8
mrozycki <mar.rozycki@gmail.com>
2014-03-06 12:47:49 +0000

    Day 4: Error

---
+++ 4-error/README.md
@@ -0,0 +1,14 @@
+Month of code
+=============
+
+Day 4: Error
+------------
+
+Displays green text in the middle of a black screen. The text flashes in
+random colors, jumps randomly and changes some of the letters. All the
+effects are random and occur in random intervals.
+
+Default text is "ERROR", but you can provide your own one after the hash,
+e.g. [QUESTION AUTHORITY](http://mrozycki.github.io/month-of-code/4-error#question authority).
+Bear in mind that the text is capitalized.
+
---
+++ 4-error/error.js
@@ -0,0 +1,65 @@
+var elem = document.getElementById("error");
+var hash = window.location.hash;
+
+if (hash !== "" && hash !== "#") {
+  elem.innerText = hash.slice(1);
+}
+
+function displacement() {
+  var x = Math.random()*100-50;
+  var y = Math.random()*100-50;
+
+  var oldy = elem.getBoundingClientRect().top+75;
+
+  elem.style.top = y+oldy+"px";
+  elem.style.left = x+"px";
+
+  setTimeout(function() {
+    elem.style.top = oldy+"px";
+    elem.style.left = 0;
+    setTimeout(function() {
+      displacement();
+    }, Math.random()*1000);
+  }, Math.random()*200);
+}
+setTimeout(displacement, Math.random()*1000);
+
+function letterSwap() {
+  var text = elem.innerText;
+  var letters = text.split("");
+  var number = Math.floor(Math.random()*letters.length);
+
+  for (var i = 0; i < number; i++) {
+    var index = Math.floor(Math.random()*letters.length);
+    var letter = String.fromCharCode(Math.floor(Math.random()*(126-33)+33));
+    letters[index] = letter;
+  }
+
+  elem.innerText = letters.join("");
+
+  setTimeout(function() {
+    elem.innerText = text;
+    setTimeout(function() {
+      letterSwap();
+    }, Math.random()*1000);
+  }, Math.random()*200);
+}
+setTimeout(letterSwap, Math.random()*1000);
+
+function colorChange() {
+  var color = elem.style.color;
+  var newcolor = "";
+  while (newcolor.length < 6) {
+   newcolor = Math.floor(Math.random()*0x1000000).toString(16);
+  }
+  elem.style.color = "#"+newcolor;
+
+  setTimeout(function() {
+    elem.style.color = color;
+    setTimeout(function() {
+      colorChange();
+    }, Math.random()*1000);
+  }, Math.random()*200);
+}
+setTimeout(colorChange, Math.random()*1000);
+
