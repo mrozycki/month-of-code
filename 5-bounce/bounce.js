10fc8c5988d44c0b3359e08d8f93960e84609450
mrozycki <mar.rozycki@gmail.com>
2014-03-06 19:41:10 +0000

    Points in bounce

--- 5-bounce/bounce.js
+++ 5-bounce/bounce.js
@@ -1,3 +1,54 @@
+function getCookie(cookie) {
+  var name = cookie+"=";
+  var cookies = document.cookie.split(";");
+  for (var i = 0; i < cookies.length; i++) {
+    var trimmed = cookies[i].trim();
+    if (trimmed.indexOf(name)===0) {
+      return cookie.substring(name.length, trimmed.length);
+    }
+  }
+  return "";
+}
+
+function Game() {
+  this.points = 0;
+
+  if (getCookie("best")==="") this.best = 0;
+  else this.best = Number(getCookie("best"));
+
+  this.updateBest();
+}
+
+Game.prototype.score = function() {
+  this.points ++;
+  this.updatePoints();
+};
+
+Game.prototype.lose = function() {
+  if (this.points === 0) return;
+  
+  if (this.points > this.best) {
+    this.best = this.points;
+    this.updateBest();
+  }
+
+  this.points = 0;
+  this.updatePoints();
+};
+
+Game.prototype.updatePoints = function() {
+  document.getElementById("score").innerHTML = this.points;
+};
+
+Game.prototype.updateBest = function() {
+  document.cookie = "best="+this.best+";"+
+    "expires="+new Date(new Date().getTime()+1000*60*60*24*365*10);
+
+  document.getElementById("best").innerHTML = "Best: "+this.best;
+};
+
+var game = new Game();
+
 function Vector(x, y) {
   this.x = x;
   this.y = y;
@@ -11,13 +62,23 @@ Vector.prototype.scale = function(k) {
   return new Vector(this.x*k, this.y*k);
 };
 
+if (typeof Math.sign == "undefined") {
+  Math.sign = function(v) {
+    if (v > 0) return -1;
+    else if (v < 0) return 1;
+    else if (v === 0) return 0;
+    else return NaN;
+  };
+}
+
 function Ball(elem) {
   var ball = this;
   this.element = elem;
   this.element.onclick = function(e) {
-    var x = -(e.offsetX-50)/50*2000;
-    var y = -(e.offsetY-50)/50*2000;
+    var x = -(e.offsetX-50)/50*500;
+    var y = Math.sign((e.offsetY-50)/50)*500;
     ball.velocity = new Vector(x,y);
+    game.score();
   };
 
   this.acceleration = new Vector(0,1000);
@@ -37,16 +98,11 @@ Ball.prototype.tick = function(t) {
   this.velocity = this.velocity.add(this.acceleration.scale(t));
   this.position = this.position.add(this.velocity.scale(t));
 
-  if (this.position.y < minY) {
-    this.position.y = 2*minY-this.position.y;
-    this.velocity.y = -this.velocity.y*0.7;
-    this.velocity.x = this.velocity.x*0.95;
-  }
-
   if (this.position.y > maxY) {
     this.position.y = 2*maxY-this.position.y;
     this.velocity.y = -this.velocity.y*0.7;
     this.velocity.x = this.velocity.x*0.95;
+    game.lose();
   }
 
   if (this.position.x < minX) {
